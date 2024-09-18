cd7840366a7b058f08cb0f0c9b9ce7075789a400
kayque vieira <kayquemantovanini25@gmail.com>
2024-09-18 12:13:12 -0300

    nota final aluno

---
+++ s7a2.py
@@ -0,0 +1,8 @@
+# Solicita a nota final do aluno
+nota = float(input("Digite a nota final do aluno: "))
+
+# Verifica se a nota é maior ou igual a 7
+if nota >= 7:
+    print("Parabéns! Você foi aprovado.")
+else:
+    print("Infelizmente, você não foi aprovado. Estude mais e tente novamente!")
